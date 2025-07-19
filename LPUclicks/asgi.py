f4af4e5e36e60ad8cc3d4871591cee46d41fc35a
Keshav-Khandelwal <keshavkhandelwal.jwr@gmail.com>
2025-07-20 01:50:33 +0530

    first commit

---
+++ LPUclicks/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for LPUclicks project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LPUclicks.settings')
+
+application = get_asgi_application()
